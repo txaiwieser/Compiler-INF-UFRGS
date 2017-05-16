5e0f10a132917d1d1d582d98eac23ca3336ce0f1
Txai Wieser <txaidw@gmail.com>
2017-05-16 20:13:26 -0300

    added function to write a string to a file

--- step_3/main.c
+++ step_3/main.c
@@ -7,6 +7,18 @@ extern int  getLineNumber();
 extern void initMe();
 extern int  isRunning();
 
+
+int writeStringToFile(char* filePath, char* string) {
+	FILE* f;
+	if (f = fopen(filePath, "w")) {
+		fprintf(f, "%s", string);
+		return 1;
+	} else {
+		fprintf(stderr, "ERROR: Couldn't open %s\n", filePath);
+		exit(1);
+	}
+}
+
 int main(int argc, char ** argv) {
 	FILE* file;
 	if (!(file = fopen(argv[1], "r"))) {

30b16ed57f2617db5b5205cadf4efbd17ba1a664
Albert Kolberg <albert@kolberg.com.br>
2017-06-21 18:20:59 -0300

    tac update

--- step_5/tac.c
+++ step_5/tac.c
@@ -1,6 +1,13 @@
 #include "tac.h"
 #include "hash.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+// ATTENTION: must math with tac_type_e at tac.h.
+char *tac_type_str[] = { //@TODO: this is only the template.
+	"TAC_LABEL",
+	"TAC_BEGINFUN"
+};
 
 tac_t *tac_node_create(tac_type_t type, hash_node_t *res, hash_node_t *op1, hash_node_t *op2) {
 
@@ -18,12 +25,54 @@ tac_t *tac_node_create(tac_type_t type, hash_node_t *res, hash_node_t *op1, hash
 
 tac_t *tac_head_node(tac_t *node) {
 	tac_t *i;
-	for(i = node; i->prev == NULL; i = i->prev);
+	for(i = node; i->prev; i = i->prev);
 	return i;
 }
 
 tac_t *tac_tail_node(tac_t *node) {
 	tac_t *i;
-	for(i = node; i->next == NULL; i = i->next);
+	for(i = node; i->next; i = i->next);
 	return i;
+}
+
+tac_t *tac_code_join(tac_t *c1, tac_t *c2) {
+	tac_t *i;
+	if(!c1 && !c2) return NULL; else if(!c1) return c2; else if(!c2) return c1;
+	for(i = c2; i->prev; i = i->prev);
+	i->prev = c1;
+	c1->next = i;
+	return c2;
+}
+
+tac_t *tac_code_reverse_copy(tac_t *node) {
+	tac_t *new_tac_node = tac_node_create(node->type, node->res, node->op1, node->op2);
+	if(!node->prev) return new_tac_node;
+	else return tac_code_join(new_tac_node, tac_code_reverse_copy(node->prev));
+}
+
+void tac_code_print_forward(tac_t *head_node) {
+	for(tac_t *i = head_node; i; i = i->next)
+		printf("TAC(%d)\n", i->type);
+}
+
+void tac_code_print_backward(tac_t *tail_node) {
+	for(tac_t *i = tail_node; i; i = i->prev)
+		printf("TAC(%d)\n", i->type);
+}
+
+void tac_test() {
+
+	tac_t *tac[10];
+	
+	for(int i = 0; i<10; i++)
+		tac[i] = tac_node_create(i, NULL, NULL, NULL);
+
+	tac_t *aux_1 = tac_code_join(tac[0], tac_code_join(tac[1], tac_code_join(tac[2], tac_code_join(tac[3], tac[4]))));
+	tac_t *aux_2 = tac_code_join(tac[5], tac_code_join(tac[6], tac_code_join(tac[7], tac_code_join(tac[8], tac[9]))));
+
+	tac_code_print_backward(tac_code_join(aux_1, aux_2));
+	printf("\n");
+	tac_code_print_forward(tac[0]);
+
+	printf("%d - %s\n", TAC_BEGINFUN, tac_type_str[TAC_BEGINFUN]);
 }
--- step_5/tac.h
+++ step_5/tac.h
@@ -3,6 +3,49 @@
 
 #include "hash.h"
 
+enum tac_type {
+	TAC_LABEL = 0,
+	//TAC_SYMBOL,	// <tac_temp>: symbol name, data_type, data_value	// parecido com TAC_VARDEC, só que declara constantes
+
+	TAC_BEGINFUN,	// <tac_temp>: symbol (identifier) of the function, N_args, return type
+	TAC_ENDFUN,		//
+	TAC_CALL,		// <tac_label>: function label to jump to, N_args, <tac_temp>: where to put function results
+	//TAC_PARAM,	// <tac_label>: parent function, param_number, data_type
+	TAC_ARG,		// position (left to right), <tac_temp>: temporary symbol with argument value
+
+	//TAC_RETURN,	// Não vai haver um TAC_RETURN, mas sim um TAC_MOVE
+
+	TAC_VARDEC,		// <tac_temp>, data_type, init_value: símbolo com o literal
+	TAC_VECDEC,		// <tac_temp>, data_type, size
+	TAC_VEC_INIT,	// <tac_temp>, position, data_value // seta valor do vetor para cada posição
+
+	TAC_MOVE,		// <tac_temp>: destination, (<tac_temp>: offset (<tac_temp>: source is a pointer))
+	//TAC_CHG_TYPE,	// <tac_temp>: destination, <tac_temp>: original, data_type: new data type to convert to
+	TAC_WRITE_VEC,
+	TAC_READ_VEC,
+	TAC_ADD,		// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_SUB,		// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_MUL,		// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_DIV,		// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+
+	TAC_EQ,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_NE,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_GT,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_GE,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_LT,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_LE,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+
+	TAC_AND,		// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_OR,			// <tac_temp>: destination, <tac_temp>: op1, <tac_temp>: op2
+	TAC_NOT,		// <tac_temp>: destination, <tac_temp>: op1
+
+	TAC_GOTO,		// <tac_label>: where to jump
+	TAC_JMP_F,		// <tac_label>: where to jump, <tac_temp>: the symbol which is compared with false
+
+	TAC_READ,		// <tac_temp>: label to put read value
+	TAC_PRINT		// <tac_temp>: variável com o dado para imprimir, data_type
+};
+
 typedef int tac_type_t;
 typedef struct tac_s {
 
@@ -15,6 +58,6 @@ typedef struct tac_s {
 
 } tac_t;
 
-tac_t *tac_node_create(tac_type_t type, hash_node_t *res, hash_node_t *op1, hash_node_t *op2);
+void tac_test();
 
 #endif
